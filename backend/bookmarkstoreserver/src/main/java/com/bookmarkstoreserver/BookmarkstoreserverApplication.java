7773df515652bb921d9c0766f9bbfad1b3e69e7f
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2022-02-11 09:02:50 +0100

    Task 1: first commit & create spring boot project

---
+++ backend/bookmarkstoreserver/src/main/java/com/bookmarkstoreserver/BookmarkstoreserverApplication.java
@@ -0,0 +1,13 @@
+package com.bookmarkstoreserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmarkstoreserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookmarkstoreserverApplication.class, args);
+    }
+
+}
---
+++ backend/bookmarkstoreserver/src/test/java/com/bookmarkstoreserver/BookmarkstoreserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookmarkstoreserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmarkstoreserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
