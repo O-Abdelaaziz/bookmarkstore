76741a175c15c14cb3a4c8f4e007294d8d4951d7
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2022-02-11 11:28:21 +0100

    Task 8: create category repository

---
+++ backend/bookmarkstoreserver/src/main/java/com/bookmarkstoreserver/repositoy/CategoryRepository.java
@@ -0,0 +1,17 @@
+package com.bookmarkstoreserver.repositoy;
+
+import com.bookmarkstoreserver.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @Created 11/02/2022 - 11:27
+ * @Package com.bookmarkstoreserver.repositoy
+ * @Project bookmarkstoreserver
+ * @User LegendDZ
+ * @Author Abdelaaziz Ouakala
+ **/
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Category findByUid(String uid);
+}
